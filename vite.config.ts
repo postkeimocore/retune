1bda55e2c5a691447692a2f668e358779d5735ea
postkeimocore <coveremo@gmail.com>
2026-09-06 07:01:42 +0900

    ci: prepare RETUNE for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react';
 import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
+  base: './',
   plugins: [react(), VitePWA({ registerType: 'autoUpdate', manifest: false })],
 });
