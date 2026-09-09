d36c4080888dbb226f1ee5bf6f684c8599c813a4
postkeimocore <coveremo@gmail.com>
2026-09-10 05:24:05 +0900

    feat: add musical pitch math

---
+++ RETUNE/Music/NoteMath.swift
@@ -0,0 +1,19 @@
+import Foundation
+
+enum NoteMath {
+    static func frequency(midi: Int, a4: Double = 440.0) -> Double {
+        a4 * pow(2.0, Double(midi - 69) / 12.0)
+    }
+
+    static func midi(frequency: Double, a4: Double = 440.0) -> Double {
+        69.0 + 12.0 * log2(frequency / a4)
+    }
+
+    static func cents(detectedHz: Double, targetHz: Double) -> Double {
+        1200.0 * log2(detectedHz / targetHz)
+    }
+
+    static func frequency(midiOffset: Int, from referenceHz: Double) -> Double {
+        referenceHz * pow(2.0, Double(midiOffset) / 12.0)
+    }
+}
